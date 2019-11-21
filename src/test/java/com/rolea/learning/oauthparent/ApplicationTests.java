5bb91b7b692ea74fcc0e94675be69fa2507d6a27
Alex Rolea <alexrolea93@gmail.com>
2019-11-21 20:10:41 +0200

    Initial commit

---
+++ src/test/java/com/rolea/learning/oauthparent/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolea.learning.oauthparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
