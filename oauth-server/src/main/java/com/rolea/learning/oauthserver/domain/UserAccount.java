a1c14ab2afe6fccef29bfd1c3c09990182535e62
Alex Rolea <alexrolea93@gmail.com>
2019-11-23 08:19:51 +0200

    Added persistence layer

---
+++ oauth-server/src/main/java/com/rolea/learning/oauthserver/domain/UserAccount.java
@@ -0,0 +1,43 @@
+package com.rolea.learning.oauthserver.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "user_account")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "facebook_id")
+    private String facebookId;
+
+    @Column(name = "google_id")
+    private String googleId;
+
+    @Column(name = "enabled")
+    private Boolean enabled;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private List<UserRole> userRoles;
+
+}
---
+++ oauth-server/src/main/java/com/rolea/learning/oauthserver/persistence/DataPopulator.java
@@ -0,0 +1,49 @@
+package com.rolea.learning.oauthserver.persistence;
+
+import com.rolea.learning.oauthserver.domain.UserAccount;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static com.rolea.learning.oauthserver.domain.UserRole.ROLE_ADMIN;
+import static com.rolea.learning.oauthserver.domain.UserRole.ROLE_USER;
+import static java.util.Arrays.asList;
+
+@Configuration
+public class DataPopulator {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DataPopulator.class);
+
+    @Autowired
+    private UserAccountRepository repository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Bean
+    public CommandLineRunner initDatabase(){
+        return (args) -> {
+            LOG.info("Starting DB init");
+            repository.save(UserAccount.builder()
+                    .email("alexrolea1@gmail.com")
+                    .password(passwordEncoder.encode("password"))
+                    .userRoles(asList(ROLE_USER))
+                    .enabled(Boolean.TRUE)
+                    .build());
+            repository.save(UserAccount.builder()
+                    .email("alexrolea93@gmail.com")
+                    .password(passwordEncoder.encode("password"))
+                    .userRoles(asList(ROLE_ADMIN))
+                    .enabled(Boolean.TRUE)
+                    .build());
+            repository.findAll()
+                    .forEach(System.out::println);
+            LOG.info("Finished DB init");
+        };
+    }
+
+}
