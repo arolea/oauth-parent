76f5b0deeb8dd4276a954f7d9853ba5951a479e1
Alex Rolea <alexrolea93@gmail.com>
2019-11-23 07:58:30 +0200

    Initial commit for oauth server

--- oauth-client/src/main/java/com/rolea/learning/oauthclient/controller/OAuthController.java
+++ oauth-client/src/main/java/com/rolea/learning/oauthclient/controller/OAuthController.java
@@ -1,4 +1,4 @@
-package com.rolea.learning.oauthparent.controller;
+package com.rolea.learning.oauthclient.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
---
+++ oauth-server/src/main/java/com/rolea/learning/oauthserver/OAuthServer.java
@@ -0,0 +1,13 @@
+package com.rolea.learning.oauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthServer.class, args);
+    }
+
+}
